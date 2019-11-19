6820b680de691f6423a2785d59d0c952a3b26097
Sushovan Samal <sushovan.samal@publicissapient.com>
2019-11-19 17:18:46 +0530

    updated stream:

---
+++ src/main/java/com/demo/tweet/controller/WebSocketEventController.java
@@ -0,0 +1,39 @@
+package com.demo.tweet.controller;
+
+import com.demo.tweet.service.StreamTweetEventService;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Controller
+@RequestMapping("/")
+public class WebSocketEventController {
+	    
+    private StreamTweetEventService streamTweetEventService;
+    
+    private List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+
+    public WebSocketEventController(StreamTweetEventService streamTweetEventService) {
+        this.streamTweetEventService = streamTweetEventService;
+    }
+    
+    @RequestMapping("/")
+    public String streamTweetAsEvents(){
+        return "events";
+    }  
+    
+    @RequestMapping("/tweetLocation")
+    public SseEmitter streamTweets() throws InterruptedException{
+    	
+    	SseEmitter sseEmitter = new SseEmitter();
+    	emitters.add(sseEmitter);
+    	sseEmitter.onCompletion(() -> emitters.remove(sseEmitter));
+    	
+    	streamTweetEventService.streamTweetEvent(emitters);
+    	
+    	return sseEmitter;
+    }
+
+}
